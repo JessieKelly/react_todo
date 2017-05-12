743ee214c09df835f1697fdc89ef635dc5ae6f04
JessieKelly <jessie@culpafree.com>
2017-05-12 18:56:18 -0500

    temporary success message

--- src/App.js
+++ src/App.js
@@ -39,7 +39,12 @@ class App extends Component {
       errorMessage: ''
     })
     createTodo(newTodo)
-      .then(() => console.log('Todo added'))
+      .then(() => this.showTempMessage('Todo added'))
+  }
+
+  showTempMessage = (msg) => {
+    this.setState({message: msg})
+    setTimeout(() => this.setState({message: ''}), 2500)
   }
 
   handleEmptySubmit(evt) {
@@ -79,6 +84,7 @@ handleRemove = (id, evt) => {
         </div>
         <div className="Todo-App">
           {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          {this.state.message && <span className='success'>{this.state.message}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
                     currentTodo={this.state.currentTodo}
                     handleSubmit={submitHandler} />
